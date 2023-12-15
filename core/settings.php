c4a0fb82d47cf2f400587a9f61a29f324e8bbd4a
shivi1526 <78632686+shivi1526@users.noreply.github.com>
2023-12-15 19:04:05 +0530

    commit3

---
+++ core/settings.php
@@ -0,0 +1,16 @@
+<?php
+# MySQL db host name
+$sql_db_host = "localhost";
+
+# MySQL db User
+$sql_db_user = "root";
+
+# MySQL db Password
+$sql_db_pass = "Lavneet@44";
+
+# MySQL db Name
+$sql_db_name = "insta_db";
+
+# Site URL
+$site_url    = "http://localhost/ColibriSM"; // E.g (https://www.colibrism.com)
+?>
